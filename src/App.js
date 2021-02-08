e1241ec7fa448987c6544f309ec0ba8bebdb517e
helloworld-hellohyeon <helloworld.hellohyeon@gmail.com>
2021-02-08 16:50:57 +0900

    Poster 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './reset.scss';
 import './common.scss';
 import style from './App.scss';
 import Header from './Component/Header/Header';
+import Poster from './Component/Poster/Poster';
 
 const cx = classNames.bind(style);
 
@@ -11,6 +12,21 @@ function App() {
   return (
     <div className={cx('app-wrapper')}>
       <Header></Header>
+      <div className={cx('poster-wrapper')}>
+        <Poster></Poster>
+        <Poster></Poster>
+      </div>
+      <div className={cx('poster-wrapper')}>
+        <Poster></Poster>
+        <Poster></Poster>
+      </div>
+      <div className={cx('poster-wrapper')}>
+        <Poster></Poster>
+        <Poster></Poster>
+      </div>
+      <div className={cx('poster-wrapper')}>
+        <Poster></Poster>
+      </div>
     </div>
   );
 }
